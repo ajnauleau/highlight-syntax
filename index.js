ca835606a80662f5d934f96d755a33ce3b17a0e0
Antoine Nauleau <ajnauleau@gmail.com>
2020-04-24 17:01:49 +0200

    Fix image

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Highlight = (string) => {
     string = string.replace(/>/g, '&gt;');
     string = string.replace(/=(["|'](.*?)["|'])/g, '=<span class="string">$1</span>');
     string = string.replace(/(\s.*)(?==<)/g, ' <span class="global">$1</span>');
-    string = string.replace(/(?!<span\sclass=".*">.*<\/span>)(&lt;[^\s&gt;]+)/g, '<span class="method">$1</span>');
+    string = string.replace(/(?!<span\sclass=".*">.*<\/span>)(&lt;[^\s&;]+)/g, '<span class="method">$1</span>');
     string = string.replace(/(?!<span\sclass=".*">.*<\/span>)(\/?&gt;)/g, '<span class="method">$1</span>');
 
     console.log(string);
